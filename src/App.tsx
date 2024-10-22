d68ce9eaf506fa4a9ce839462e4b34334470df9b
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-10-22 09:18:23 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <>
+      <h1>hoge</h1>
+      <p className="text-blue-600 font-bold text-5xl ">
+        Hello Tailwind
+      </p>
+    </>
+  )
+}
